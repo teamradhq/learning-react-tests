b2fb3d284a1fa15d75ffa0cf436a58e2e66c88ce
Paul Beynon <paul@beynon.app>
2021-06-08 20:57:05 +1000

    useState should set default values

--- test/useState.test.tsx
+++ test/useState.test.tsx
@@ -4,18 +4,27 @@ import {
 } from "@testing-library/react";
 
 function RenderComponent() {
-  const [state, updateCounter] = useState({ counter: 0 });
+  const [state, updateState] = useState({ counter: 0, name: 'name' });
   const { counter } = state;
 
   return (
     <div data-testid="render">
-      <div data-testid="counter">{counter}</div>
-      <button data-testid="increment"
-        onClick={() => updateCounter({
-          ...state,
-          counter: state.counter + 1,
-        })}
-      >Increment</button>
+      <div className="counter--container">
+        <div data-testid="counter">{counter}</div>
+        <button data-testid="increment"
+          onClick={() => updateState({
+            ...state,
+            counter: state.counter + 1,
+          })}
+        >Increment</button>
+      </div>
+      <div className="name--container">
+        <input data-testid="name-input" value={state.name} onChange={e => updateState({
+            ...state,
+            name: e.currentTarget.value,
+          })} />
+        <div data-testid="name-element"></div>
+      </div>
     </div>
   );
 }
@@ -28,6 +37,14 @@ describe('useState', () => {
   let getByTestId;
   beforeEach(() => {
     getByTestId = renderComponent().getByTestId;
+  });
+
+  it('should have default values', async () => {
+    const input = await getByTestId('name-input');
+    const counter = await getByTestId('counter');
+
+    expect(input).toHaveValue('name');
+    expect(counter).toHaveTextContent('0');
   });
 
   it('should update state on click', async () => {
