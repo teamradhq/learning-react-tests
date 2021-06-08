20463cb805babe544e5b9c3b947e13aea7cca53c
Paul Beynon <paul@beynon.app>
2021-06-08 19:52:28 +1000

    Render stateless component

---
+++ test/renderComponent.test.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import {
+  render,
+} from "@testing-library/react";
+
+function RenderComponent() {
+  return (
+    <div data-testid="render">Render!</div>
+  );
+}
+
+function renderComponent() {
+  return render(<RenderComponent />);
+}
+
+describe('<RenderComponent>', () => {
+  it('should render a div', async () => {
+    const { getByTestId } = renderComponent();
+    const component = await getByTestId('render');
+    expect(component).toContainHTML('<div data-testid="render">Render!</div>');
+  });
+});
